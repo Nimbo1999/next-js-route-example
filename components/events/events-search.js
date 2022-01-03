3608fdd9140e533033790162666bc5bd1156bf0f
Matheus Lopes <matlopes1999@gmail.com>
2022-01-03 09:43:13 -0300

    Add the search event function

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,14 +1,29 @@
 import Button from '../ui/button';
 import classes from './events-search.module.css';
 
-function EventsSearch() {
+function EventsSearch({ onSearch }) {
+    function formatFormDataValues(formData) {
+        const result = {};
+        for (const key of formData.keys()) {
+            const value = formData.get(key);
+            result[key] = value;
+        }
+        return result;
+    }
+
+    function submitHandler(event) {
+        event.preventDefault();
+        const formValues = formatFormDataValues(new FormData(event.target));
+        onSearch(formValues);
+    }
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <div className={classes.controls}>
                 <div className={classes.control}>
                     <label htmlFor="year">Year</label>
 
-                    <select id="year">
+                    <select id="year" name="year">
                         <option value="2021">2021</option>
                         <option value="2022">2022</option>
                     </select>
@@ -16,7 +31,7 @@ function EventsSearch() {
 
                 <div className={classes.control}>
                     <label htmlFor="month">Month</label>
-                    <select id="month">
+                    <select id="month" name="month">
                         <option value="0">January</option>
                         <option value="1">February</option>
                         <option value="2">March</option>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,15 +1,33 @@
 import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
 import EventList from '../../components/events/event-list';
 import { getAllEvents } from '../../dummy-data';
 import EventsSearch from '../../components/events/events-search';
 
 function AllEventsPage() {
+    const router = useRouter();
     const events = getAllEvents();
+
+    function isFormValid(formValues) {
+        const keys = ['month', 'year'];
+        for (const key of keys) {
+            if (!formValues[key]) return false;
+        }
+        return true;
+    }
+
+    function onSearch(formKeyValues) {
+        if (isFormValid(formKeyValues)) {
+            return router.push(`/events/${formKeyValues['year']}/${formKeyValues['month']}`);
+        }
+
+        return alert('Invalid parameters!');
+    }
 
     return (
         <Fragment>
-            <EventsSearch />
+            <EventsSearch onSearch={onSearch} />
             <EventList items={events} />
         </Fragment>
     );
